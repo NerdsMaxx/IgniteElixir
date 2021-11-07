cfd2e67e6dbba30c762971cdd73d9cb1c2bb471f
Guilherme Henrique <henrique101124@gmail.com>
2021-11-06 22:33:15 -0300

    atualizacao

--- reports_generator/lib/reports_generator.ex
+++ reports_generator/lib/reports_generator.ex
@@ -2,38 +2,71 @@ defmodule ReportsGenerator do
   alias ReportsGenerator.Parser
 
   @available_foods [
-    "açai",
+    "açaí",
     "churrasco",
     "esfirra",
-    "hambúrger",
+    "hambúrguer",
     "pastel",
     "pizza",
     "prato_feito",
     "sushi"
   ]
 
+  @options [
+    "foods",
+    "users"
+  ]
+
   def build(filename) do
     filename
     |> Parser.parse_file()
-    |> Enum.reduce(report_acc(), &sum_values(&1, &2))
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
-  # defp fetch_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
+  def build_from_many(filenames) when not is_list(filenames),
+    do: {:error, "Please, provide a list of strings"}
 
-  defp sum_values([id, food_name, price], %{foods: foods, users: users} = report) do
-    users = Map.put(users, id, users[id] + price)
+  def build_from_many(filenames) do
+    result =
+      filenames
+      |> Task.async_stream(&build/1)
+      |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+
+    {:ok, result}
+  end
+
+  def fetch_higher_cost(report, option) when option in @options,
+    do: {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+
+  def fetch_higher_cost(_report, _option), do: {:error, "Invalid option!"}
+
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users}) do
     foods = Map.put(foods, food_name, foods[food_name] + 1)
+    users = Map.put(users, id, users[id] + price)
 
-    # report
-    # |> Map.put(:users, users)
-    # |> Map.put(:foods, foods)
-    report
+    build_report(foods, users)
+  end
+
+  defp sum_reports(%{"foods" => foods1, "users" => users1}, %{
+         "foods" => foods2,
+         "users" => users2
+       }) do
+    foods = merge_maps(foods1, foods2)
+    users = merge_maps(users1, users2)
+
+    build_report(foods, users)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
   end
 
   defp report_acc do
     foods = Enum.into(@available_foods, %{}, &{&1, 0})
     users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
-    %{users: users, foods: foods}
+    build_report(foods, users)
   end
+
+  defp build_report(foods, users), do: %{"foods" => foods, "users" => users}
 end
